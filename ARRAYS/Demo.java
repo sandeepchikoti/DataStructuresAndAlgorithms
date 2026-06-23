0763e18d8f92e286f7d85e75b1bdd41a50a74efe
Sandeep-patel-17 <chikotisandeep@gmail.com>
2026-06-23 10:55:59 +0530

    Add initial implementation of Demo class with array output

---
+++ ARRAYS/Demo.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
+        System.out.println(Arrays.toString(arr));
+    }
+}
