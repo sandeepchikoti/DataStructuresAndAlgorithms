b9847d042721a2eaa04736be3877fc18c7123e2d
Sandeep-patel-17 <chikotisandeep@gmail.com>
2026-06-24 07:14:54 +0530

    Refactor Union of Two Sorted Arrays implementation to use ArrayList for optimal union and update input arrays

--- ARRAYS/UnionOfTwoSortedArrays.java
+++ ARRAYS/UnionOfTwoSortedArrays.java
@@ -1,21 +1,61 @@
-import java.util.*;
+import java.util.ArrayList;
 
 public class UnionOfTwoSortedArrays {
     public static void main(String[] args) {
-        int arr1[] = {100,20,30,40,50,50};
-        int arr2[] = {60,60,60,70,70,80,80,90};
-        TreeSet<Integer> hs = new TreeSet<>();
-        for(int i=0;i<arr1.length;i++){
-            hs.add(arr1[i]);
+        int arr1[] = {10,20,30,40,40,50};
+        int arr2[] = {20,30,30,40,50};
+
+            //BRTUE
+    //     HashSet<Integer> hs = new HashSet<>();
+    //     for(int i=0;i<arr1.length;i++){
+    //         hs.add(arr1[i]);
+    //     }
+    //     for(int i=0;i<arr2.length;i++){
+    //         hs.add(arr2[i]);
+    //     }
+    //     int union[] = new int[hs.size()];
+    //     int j = 0;
+    //     for(int i : hs){
+    //         union[j++] = i;
+    //     }
+    //     System.out.println(Arrays.toString(union));
+
+            //OPTIMAL
+    ArrayList<Integer> ls = new ArrayList<>();
+    int n1 = arr1.length;
+    int n2 = arr2.length;
+
+    int i = 0;
+    int j = 0;
+    
+    while(i<n1 && j<n2){
+        if(arr1[i]<=arr2[j]){
+            if(ls.isEmpty() || ls.get(ls.size()-1)!=arr1[i]){
+                ls.add(arr1[i]);
+            }
+            i++;
         }
-        for(int i=0;i<arr2.length;i++){
-            hs.add(arr2[i]);
+        else{
+            if(ls.isEmpty() || ls.get(ls.size()-1)!=arr2[j]){
+                ls.add(arr2[j]);
+            }  
+            j++;          
         }
-        int union[] = new int[hs.size()];
-        int j = 0;
-        for(int i : hs){
-            union[j++] = i;
-        }
-        System.out.println(Arrays.toString(union));
     }
+    while(j<n2){
+             if(ls.isEmpty() || ls.get(ls.size()-1)!=arr2[j]){
+                ls.add(arr2[j]);
+            }  
+            j++;        
+    }
+    while(i<n1){
+             if(ls.isEmpty() || ls.get(ls.size()-1)!=arr1[i]){
+                ls.add(arr1[i]);
+            }  
+            i++;  
+    }
+    System.out.println(ls);
+
+
+}
 }
